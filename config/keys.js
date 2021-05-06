f29ebd8bbc3d76154b47dff403630b45b151a58d
Elmer Mendoza <mdelefm@gmail.com>
2021-05-06 15:46:25 -0600

    created Server mongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://mdel_efm:Nov14151@mernshopping.jrbae.mongodb.net/mernShopping?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get All Items
+// @access Public
+
+router.get ('/',(req,res) => {
+    Item.find()
+        .sort({dates: -1})
+        .then(items => res.json(items))
+});
+
+// @route POST api/items
+// @desc Create An Item
+// @access Public
+
+router.post ('/',(req,res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id
+// @desc Delete An Item
+// @access Public
+
+// router.delete ('/:id',(req,res) => {
+//     Item.findById(req.params.id)
+//         .then(item => item.remove()
+//         .then(() => res.json({success:true})))
+//         .catch(err => res.status(404).json({success:false}));
+// });
+router.delete ('/:id',(req,res) => {
+    Item.findByIdAndRemove(req.params.id)
+        .then(() => res.json({success:true}))
+        .catch(err => res.status(404).json({success:false}));
+});
+
+// @route UPDATE api/items/:id
+// @desc Update An Item
+// @access Public
+
+router.put ('/:id',(req,res) => {
+    Item.findByIdAndUpdate(req.params.id,req.body)
+        .then(() => res.json({success:true}))
+        .catch(err => res.status(404).json({success:false}));
+});
+
+module.exports = router;
